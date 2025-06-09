546ee91edbb68f0396489af3f404f4506c81a15c
Nakzylz <kec21d@acu.edu>
2025-06-09 16:58:24 -0500

    Commit 6 for Ch12
    Added all LeanTweens, 5 PostProcessing effects, and all elements of the character trails particle effects

--- MobileDev_KEC_DET/Assets/Scripts/MainMenuBehaviour.cs
+++ MobileDev_KEC_DET/Assets/Scripts/MainMenuBehaviour.cs
@@ -10,14 +10,23 @@ public class MainMenuBehaviour : MonoBehaviour
     /// we want to go to</param>
     /// 
     public TextMeshProUGUI highScoreText;
-    public void LoadLevel(string levelName)
+    public GameObject controlPanel;
+    public void LoadLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
     }
 
     public void Start()
     {
+        /* Unpause the game if needed */
+        Time.timeScale = 1;
+
+        // check for a high score and set it to our TMProUGUI
         GetDisplayScore();
+
+        // slide in the Panel with all the goodies
+        SlideMenuIn(controlPanel);
+
     }
 
     public void ResetScore()
@@ -29,5 +38,50 @@ public class MainMenuBehaviour : MonoBehaviour
     private void GetDisplayScore()
     {
         highScoreText.text = "High Score: " + PlayerPrefs.GetInt("score").ToString();
+    }
+
+    /// <summary>
+    /// Will move an object from the left side of the screen
+    /// to the center
+    /// </summary>
+    /// <param name="obj">The UI element we would like to
+    /// move</param>
+    public void SlideMenuIn(GameObject obj)
+    {
+        obj.SetActive(true);
+        var rt = obj.GetComponent<RectTransform>();
+        if (rt)
+        {
+            /* Set the object's position offscreen */
+            var pos = rt.position;
+            pos.x = -Screen.width / 2;
+            rt.position = pos;
+            /* Move the object to the center of the screen
+               (x of 0 is centered) */
+            var tween = LeanTween.moveX(rt, 0, 1.5f);
+            tween.setEase(LeanTweenType.easeInOutExpo);
+            tween.setIgnoreTimeScale(true);
+        }
+    }
+
+    /// <summary>
+    /// Will move an object to the right offscreen
+    /// </summary>
+    /// <param name="obj">The UI element we would like to
+    /// move </param>
+    public void SlideMenuOut(GameObject obj)
+    {
+        var rt = obj.GetComponent<RectTransform>();
+        if (rt)
+        {
+            var tween = LeanTween.moveX(rt,
+            Screen.width / 2, 0.5f);
+            tween.setEase(LeanTweenType.easeOutQuad);
+            tween.setIgnoreTimeScale(true);
+            tween.setOnComplete(() =>
+            {
+                obj.SetActive(false);
+            });
+        }
     }
 }
--- MobileDev_KEC_DET/Assets/Scripts/PauseScreenBehaviour.cs
+++ MobileDev_KEC_DET/Assets/Scripts/PauseScreenBehaviour.cs
@@ -31,7 +31,15 @@ public class PauseScreenBehaviour : MainMenuBehaviour
         /* If the game is paused, timeScale is 0,
            otherwise 1 */
         Time.timeScale = (paused) ? 0 : 1;
-        pauseMenu.SetActive(paused);
+        //pauseMenu.SetActive(paused);
+        if (paused)
+        {
+            SlideMenuIn(pauseMenu);
+        }
+        else
+        {
+            SlideMenuOut(pauseMenu);
+        }
         onScreenControls.SetActive(!paused);
     }
     void Start()
